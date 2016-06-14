bf6c22c62b6f87fd9d85625a68d69f1ab47403ca
WymA <matthias2wym@outlook.com>
2016-06-14 22:56:37 +0800

    for loops statement

--- hello-world.erl
+++ hello-world.erl
@@ -1,13 +1,12 @@
 % Hello world program
 -module('hello-world').
--export([while/1, while/2, start/0]).
+-export([for/2,start/0]).
 
-while( L ) -> while(L, 0).
-while( [], Acc ) -> Acc;
-while( [_|T], Acc ) ->
-    io:fwrite("~w~n", [Acc]),
-    while(T, Acc+1).
+for(0,_) ->
+   [];
+   for( N, Term ) when N > 0 ->
+   io:fwrite("Hello~n"),
+   [ Term | for(N-1,Term) ].
 
-start()->
-    X = [1, 2, 3, 4],
-    while(X).
+start() ->
+   for(5,1).
