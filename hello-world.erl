00311739c9939b361f36cc41688a055057e478db
WymA <matthias2wym@outlook.com>
2016-06-14 19:54:29 +0800

    Tuple size

--- hello-world.erl
+++ hello-world.erl
@@ -3,6 +3,5 @@
 -export([start/0]).
 
 start() ->
-    Bin1 = <<10, 20>>,
-    X = binary_to_list(Bin1),
-    io:fwrite("~w", [X]).
+    P = {john, 24, {june, 25}},
+    io:fwrite("~w", [tuple_size(P)]).
